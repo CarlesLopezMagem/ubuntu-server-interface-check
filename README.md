d6b33a1006c901a152968b386f3256c5d6db3bfd
CarlesLopezMagem <carlesloma@gmail.com>
2019-09-13 12:38:55 +0200

    [Feat] Added command arg

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ubuntu-server-interface-check
 
 Aquest és un repositori per controlar amb l'eina cron si una interfice està caiguda i executa una comanda
+
+## Afegir control a una interficie
+
+```bash
+chmod +x check_interface.sh
+echo "*/5 * * * * root $(pwd)/check_interface.sh INTERFICE COMMAND" << /etc/cron.d/check_interfaces
+```
--- check_interface.sh
+++ check_interface.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-if [ -z $1 ]
+if [ -z $1 ] || [ -z $2 ]
 then
 	echo "ERROR: INTERFACE must be done."
-	echo "Use: sh check_interface.sh INTERFACE"
+	echo "Use: check_interface.sh INTERFACE COMMAND"
 	return
 fi
 
 INTERFACE=$1
+COMMAND=$2
 echo "Selected interface: $INTERFACE"
 STATUS=$(cat /sys/class/net/$INTERFACE/operstate)
 if [ $STATUS != "up" ]
 then
-	echo "$INTERFACE is down. Start reboot"
-	reboot
+	echo "$INTERFACE is down. Execute command: $COMMAND"
+	bash -c $COMMAND
 	return
 fi
 echo "ALL OK"
